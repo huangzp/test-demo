ca5110848aac386ffd8232b213729c64ed47d3ab
huangzp <271967835@qq.com>
2024-05-30 17:35:48 +0800

    第一次提交

---
+++ src/main/java/com/hzp/testdemo/java/TestJavaService.java
@@ -0,0 +1,13 @@
+package com.hzp.testdemo.java;
+
+public class TestJavaService {
+
+    public int test(int step){
+        int j = 0;
+        int i = 0;
+        for(;;i ++){
+            j += step;
+        }
+        return j;
+    }
+}
